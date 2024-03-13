df0d4e076d88aa603a8bc33dda34daee3f5db4d1
Stefan Andrei <32457539+canyouseethroughme@users.noreply.github.com>
2024-03-13 22:50:00 +0100

    updated project for logistics

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,14 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <code>DFDS Logistics Monorepo Documentation</code>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/dfds-frontend/logistics",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Logistics Monorepo Documentation",
   },
-}
+};
 
-export default config
+export default config;
